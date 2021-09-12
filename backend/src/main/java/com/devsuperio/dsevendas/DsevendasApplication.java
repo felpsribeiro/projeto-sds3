8da828e9192fedd387a95589f8d362ce6625efc2
Felipe Ribeiro <felipemch14@gmail.com>
2021-09-12 19:35:01 -0300

    Projetos criados

---
+++ backend/src/main/java/com/devsuperio/dsevendas/DsevendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperio.dsevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperio/dsevendas/DsevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperio.dsevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
