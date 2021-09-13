73f5f77c050d2c0b766788229e3c3fc2e96c6898
Felipe Ribeiro <felipemch14@gmail.com>
2021-09-13 01:36:52 -0300

    Pagination

--- backend/src/main/java/com/devsuperio/dsevendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperio/dsevendas/controllers/SaleController.java
@@ -1,26 +1,26 @@
 package com.devsuperio.dsevendas.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperio.dsevendas.dto.SellerDTO;
-import com.devsuperio.dsevendas.service.SellerService;
+import com.devsuperio.dsevendas.dto.SaleDTO;
+import com.devsuperio.dsevendas.service.SaleService;
 
 @RestController
-@RequestMapping(value = "/sellers")
-public class SellerController {
+@RequestMapping(value = "/sales")
+public class SaleController {
 	
 	@Autowired
-	private SellerService service;
+	private SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<SellerDTO>> findAll() {
-		List<SellerDTO> list = service.findAll();
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 	}
 }
---
+++ backend/src/main/java/com/devsuperio/dsevendas/service/SaleService.java
@@ -0,0 +1,30 @@
+ package com.devsuperio.dsevendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperio.dsevendas.dto.SaleDTO;
+import com.devsuperio.dsevendas.entilies.Sale;
+import com.devsuperio.dsevendas.repostitories.SaleRepository;
+import com.devsuperio.dsevendas.repostitories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+	
+}
